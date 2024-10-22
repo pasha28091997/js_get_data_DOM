ac8ba5bd3e9e7f747526609452d0ae35551f4378
Pasha Shchukin <pasha4320692@gmail.com>
2024-10-22 23:00:36 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const element = document.getElementsByClassName('population');
+let allPopulationNumber = 0;
+
+for (const key of element) {
+  allPopulationNumber += +key.innerHTML.replaceAll(',', '');
+}
+
+const averagePopulation = allPopulationNumber / element.length;
+
+const classTotalPopulation = document.querySelector('.total-population');
+
+classTotalPopulation.innerHTML = allPopulationNumber;
+
+const classAveragePopulation = document.querySelector('.average-population');
+
+classAveragePopulation.innerHTML = averagePopulation;
